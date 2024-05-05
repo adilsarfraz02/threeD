02750d97f6ffa915e26cb150dc128a878db71ca8
Muhammad ADil <adilsarfr00@gmail.com>
2024-05-05 21:02:24 +0500

    hi

--- app/components/UiProvider.jsx
+++ app/components/UiProvider.jsx
@@ -4,21 +4,21 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 export function UiProvider({ children }) {
   const router = useRouter();
-   const onBlur = () => {
-     document.title = "Come Back! Next.js";
-   };
-   const onFocus = () => {
-     document.title = "Next.js - Three Js";
-   };
+  const onBlur = () => {
+    document.title = "Come Back! Next.js";
+  };
+  const onFocus = () => {
+    document.title = "Next.js - Three Js";
+  };
 
-   useEffect(() => {
-     window.addEventListener("blur", onBlur);
-     window.addEventListener("focus", onFocus);
+  useEffect(() => {
+    document.addEventListener("blur", onBlur);
+    document.addEventListener("focus", onFocus);
 
-     return () => {
-       window.removeEventListener("blur", onBlur);
-       window.removeEventListener("focus", onFocus);
-     };
-   }, []);
+    return () => {
+      document.removeEventListener("blur", onBlur);
+      document.removeEventListener("focus", onFocus);
+    };
+  }, []);
   return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
 }
